bb0acd54d2e43061efbbe621366ef3af6899d373
greystone22 <89351647+greystone22@users.noreply.github.com>
2023-05-27 14:58:28 -0400

    Add files via upload

---
+++ Find_Documents.py
@@ -0,0 +1,18 @@
+# greystone22
+# 05/27/2023 14:44
+
+import os
+
+# Path to the Documents folder
+documents_folder = os.path.expanduser("~/Documents")
+
+# List all files in the Documents folder
+file_list = []
+for root, dirs, files in os.walk(documents_folder):
+    for file in files:
+        file_path = os.path.join(root, file)
+        file_list.append(file_path)
+
+# Print the list of installed files
+for file_path in file_list:
+    print(file_path)
